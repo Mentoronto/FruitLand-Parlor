8801d80076b356c4df0ff66dabf8983022cedc95
Mentoronto <mentorberbatov@gmail.com>
2022-02-07 12:38:30 -0500

    Cash Order Details

--- component/OrderDetails.jsx
+++ component/OrderDetails.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import styles from "../styles/OrderDetails.module.css";
+import styles from "../styles/OrderDetails.module.css"
 
-const OrderDetails = () => {
+const OrderDetails = ({total, createOrder}) => {
   const [customer,setCustomer] = useState("");
   const [address,setAddress] = useState("");
+
+  const handleClick = () =>{
+    createOrder({ customer,address,total,method:0 });
+  }
   return (
   <div className={styles.container}>
     <div className={styles.wrapper}>
@@ -12,8 +16,17 @@ const OrderDetails = () => {
         <label className={styles.label}>Name Surname</label>
         <input type="text" placeholder="John Doe" className={styles.input} onChange={(e)=>setCustomer(e.target.value)}/>
       </div>
+      <div className={styles.item}>
+        <label className={styles.label}>Phone Number</label>
+        <input type="text" placeholder="111-111-1111" className={styles.input} />
+      </div>
+      <div className={styles.item}>
+        <label className={styles.label}>Address</label>
+        <textarea type="text" placeholder="123 YellowBourne Rd"  className={styles.textarea} onChange={(e)=>setAddress(e.target.value)}/>
     </div>
-  </div>
+    <button onClick={handleClick} className={styles.button}>Order</button>
+      </div>
+    </div>
   );
 };
 
